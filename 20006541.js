bed48979e6753fa7f6fba6881f67b7d5d6ea796d
Donaldo <donaldomerida40@gmail.com>
2020-02-07 00:06:43 -0600

    version 1

---
+++ 20006541.js
@@ -0,0 +1,44 @@
+var mes=10;
+
+switch(mes) {
+case 1:
+alert("invierno");
+break;
+case 2:
+alert("invierno");
+break;
+case 3:
+alert("primavera");
+break;
+case 4:
+alert("primavera");
+break;
+case 5:
+alert("primavera");
+break;
+case 6:
+alert("verano");
+break;
+case 7:
+alert("verano");
+break;
+case 8:
+alert("verano");
+break;
+case 9:
+alert("otoño");
+break;
+case 10:
+alert("otoño");
+break;
+case 11:
+alert("otoño");
+break;
+case 12:
+alert("invierno");
+break;		
+}
+var carnet="20006541";
+
+document.write('Mi carnet es: 20006541, Donaldo Albertino Mérida Mérida');
+
